1eca8dd6138a459a1c045d061d7a18355605e39e
CJ3016374 <CJ3016374@LAB01-PC27>
2023-04-18 16:06:39 -0300

    Adicionar arquivos de projeto.

---
+++ atividade 5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int numero;
+            Console.WriteLine("digite um numero.");
+            numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+                 Console.WriteLine(i);
+            Console.ReadKey();
+            */
+
+
+            /*
+            int numero;
+            Console.WriteLine("digite um numero");
+            numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= numero; i++) 
+                if (i % 2 == 0) 
+                    Console.WriteLine(i);
+            Console.ReadKey();
+            */
+
+            /*
+            int n;
+            Console.WriteLine("digite um numero");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1000; i >= n; i--) 
+            if ( i % 2 == 0)
+                Console.WriteLine(i);
+            Console.ReadKey();
+            */
+
+
+            /*
+            int n1, n2, soma = 0;
+            Console.WriteLine("digite um numero");
+            n1 = int.Parse(Console.ReadLine());
+            for (; n1 <= 200;)
+            {
+                Console.WriteLine("digite o segundo numero");
+                n2 = int.Parse(Console.ReadLine());
+                if (n2 > 0)
+                    n1 = n1 + n2;
+                Console.WriteLine(n1);
+              
+            }
+            Console.ReadKey();
+            */
+
+
+            int n;
+            Console.WriteLine("digite um numero");
+            n = int.Parse(Console.ReadLine());
+            for (int i = i; i <= n; i++)
+            { 
+
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
